b828b9098838649b1af115313794e803945a0cc5
pfelto <paul.r.felton@gmail.com>
2022-03-02 15:55:52 -0500

    Passing the freeCodeCamp tests but going to work on a few things before submitting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { marked } from "marked";
 
 function App() {
   const [markup, setMarkup] = useState(
-    `# Welcome to my React Markdown Previewer!`
+    "# Welcome to my React Markdown Preview\n## This is a sub-heading\n### And here's some other cool stuff\nHeres some code, `<div></div>`, between 2 backtics.\n```\n\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine){\n\tif (firstLine == '```' && lastLine == '```'){\n\treturn multiLineCode;\n\t}\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n- Some are bulleted.\n- With different indentation levels.\n- That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)"
   );
   return (
     <div
