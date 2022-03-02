362792093f6dc2d8e82f3d04453fcbb2ab7a4034
pfelto <paul.r.felton@gmail.com>
2022-03-02 15:46:24 -0500

    Good progress on Markdown editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { marked } from "marked";
 
 function App() {
+  const [markup, setMarkup] = useState(
+    `# Welcome to my React Markdown Previewer!`
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        gap: "2vw",
+        minHeight: "100vh",
+        backgroundColor: "lightgrey",
+        padding: "4vh 2vw",
+      }}
+    >
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          padding: "5px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "1vh",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h3 style={{ alignSelf: "center" }}>Input</h3>
+          <button style={{ alignSelf: "center" }} onClick={() => setMarkup("")}>
+            Clear
+          </button>
+        </div>
+        <textarea
+          id="editor"
+          style={{
+            flex: 1,
+            border: "1px solid black",
+            backgroundColor: "white",
+            borderRadius: "3px",
+            resize: "none",
+            overflowY: "scroll",
+            padding: "4px",
+          }}
+          value={markup}
+          onChange={(e) => setMarkup(e.target.value)}
+        ></textarea>
+      </div>
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          padding: "5px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "1vh",
+          }}
+        >
+          <h3 style={{ alignSelf: "center" }}>Preview</h3>
+        </div>
+        <div
+          id="preview"
+          style={{
+            flex: 1,
+            border: "1px solid black",
+            backgroundColor: "white",
+            borderRadius: "3px",
+            overflowY: "scroll",
+            padding: "4px",
+          }}
+          dangerouslySetInnerHTML={{ __html: marked.parse(markup) }}
+        ></div>
+      </div>
     </div>
   );
 }
